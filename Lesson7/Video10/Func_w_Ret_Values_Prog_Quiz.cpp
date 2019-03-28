38c3da79437bd174ee253c98e938ca2f8d4b7889
CP1984 <chingping1984@gmail.com>
2019-03-28 14:12:11 +0800

    Lesson 7, Video 10, Func. w Ret. Values Prog. Quiz.

---
+++ Lesson7/Video10/Func_w_Ret_Values_Prog_Quiz.cpp
@@ -0,0 +1,11 @@
+/*Goal: practice writing functions with parameters and return values.*/
+
+#include "main.hpp"
+
+int main() {
+	int m1 = 1;
+	int m2 = 2;
+	printEquation(m1, m2, sum(m1, m2), '+');
+
+	return 0;
+}
